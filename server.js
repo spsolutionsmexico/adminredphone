e6261b92362423c2c5b6632ea93ee2423bd1b241
israel125x <30633437+israel125x@users.noreply.github.com>
2017-11-06 12:54:25 -0600

    test 5 actualizar

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var condblib = require(__dirname + '/condblib');
 
+//firebase 
+var firebase = require('firebase');
+//cofiguracion conexion firebase
+var config = {
+    apiKey: process.env.FBASE_APIKEY, //"AIzaSyCsVB58GbuUmkwSSv4WAlk3FOuU786IrEg",
+    authDomain: process.env.AUTH_DOMAIN, //"cocacola-redphone.firebaseapp.com",
+    databaseURL: process.env.DATABASE_URL, //"https://cocacola-redphone.firebaseio.com",
+    projectId: process.env.PROJECT_ID, //"cocacola-redphone",
+    storageBucket: "",
+    messagingSenderId: process.env.MESSAGING_SERNDER_ID //"634647561747"
+};
+var defaultApp = firebase.initializeApp(config);
+var db = firebase.database();
+const REF_ALTA = process.env.REF_ALTA;
+const REF_ALTA_DATA = process.env.REF_ALTA_DATA;
+const REF_RETO = process.env.REF_RETO;
+
+
 var app = express();
 app.use(bodyParser.json());
 
