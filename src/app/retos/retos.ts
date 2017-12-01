2beeb6b12cdd4c5fc43bbb180ac5058aa4569ca6
israel125x <30633437+israel125x@users.noreply.github.com>
2017-12-01 14:59:30 -0600

    class retos

--- src/app/retos/retos.ts
+++ src/app/retos/retos.ts
@@ -1,2 +1,10 @@
 export class Retos {
+    idreto?: string;
+    horaenvio: string;
+    descripcion: string;
+    cantidadinvitados: number;
+    cantidadrespuestas: number;
+    fechaactualizacion: Date;
+    estado: string;
+    fechaenvio: Date;
 }
