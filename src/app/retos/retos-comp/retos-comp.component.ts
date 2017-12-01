9563997f7a77ed5e75365bb16058eac6f8c528d3
israel125x <30633437+israel125x@users.noreply.github.com>
2017-12-01 14:27:17 -0600

    componente retos

--- src/app/retos/retos-comp/retos-comp.component.ts
+++ src/app/retos/retos-comp/retos-comp.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-retos-comp',
+  selector: 'retos-comp',
   templateUrl: './retos-comp.component.html',
   styleUrls: ['./retos-comp.component.css']
 })
