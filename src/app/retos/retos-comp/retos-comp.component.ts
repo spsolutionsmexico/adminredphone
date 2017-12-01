dd87d97e3c33a5df54c4386987924cd42c135dc2
israel125x <30633437+israel125x@users.noreply.github.com>
2017-12-01 15:39:25 -0600

    test retos 1

--- src/app/retos/retos-comp/retos-comp.component.ts
+++ src/app/retos/retos-comp/retos-comp.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Retos } from '../retos';
+import { RetosServService } from '../retos-serv.service';
 
 @Component({
   selector: 'retos-comp',
   templateUrl: './retos-comp.component.html',
-  styleUrls: ['./retos-comp.component.css']
+  styleUrls: ['./retos-comp.component.css'],
+  providers: [RetosServService]
 })
 export class RetosCompComponent implements OnInit {
+  retos: Retos[]
+  selectedRetos: Retos
 
-  constructor() { }
+  constructor(private retosService: RetosServService) { }
 
   ngOnInit() {
+    this.retosService
+    .getRetos().then((retos: Retos[]) => {
+      this.retos = retos.map((retos) => {
+        return retos;
+      });
+    });
   }
 
 }
--- src/app/retos/retos-serv.service.ts
+++ src/app/retos/retos-serv.service.ts
@@ -5,6 +5,22 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class RetosServService {
 
-  constructor() { }
+  private retosUrl = '/api/retos';
+  
+      constructor (private http: Http) {}
+  
+      // get("/api/retos")
+      getRetos(): Promise<Retos[]> {
+        return this.http.get(this.retosUrl)
+                   .toPromise()
+                   .then(response => response.json() as Retos[])
+                   .catch(this.handleError);
+      }
 
+      private handleError (error: any): Promise<any> {
+        let errMsg = (error.message) ? error.message :
+        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console
+        return Promise.reject(errMsg);
+      }
 }
