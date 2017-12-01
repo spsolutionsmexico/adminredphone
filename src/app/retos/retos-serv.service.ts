8e3ad3cb9f69c74ee1cc9dcaef21bbe1bdb9744a
israel125x <30633437+israel125x@users.noreply.github.com>
2017-12-01 14:51:21 -0600

    retos compinent test 2

--- src/app/retos/retos-serv.service.ts
+++ src/app/retos/retos-serv.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-
+import { Retos } from './Retos';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class RetosServService {
 
