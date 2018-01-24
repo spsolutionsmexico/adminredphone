da16a6882f200404630b8b2e2e8e94e6037eb9ec
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-01-24 14:15:10 -0600

    1
    1

--- src/app/grafico/consume.service.ts
+++ src/app/grafico/consume.service.ts
@@ -1,26 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
-import { Data } from ./Data
+
 @Injectable()
-export class GrapServService {
+export class ConsumeService {
 
-  private GrapUrl = '/api/respuestagrap';
-  
-      constructor (private http: Http) {}
-  
-      //get("/api/respuestagrap")
-      getData(): Promise<Data[]> {
-        return this.http.get(this.GrapUrl)
-                   .toPromise()
-                   .then(response => response.json() as Data[])
-                   .catch(this.handleError);
-      }
+  constructor() { }
 
-      private handleError (error: any): Promise<any> {
-        let errMsg = (error.message) ? error.message :
-        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        console.error(errMsg); // log to console
-        return Promise.reject(errMsg);
-      }
 }
