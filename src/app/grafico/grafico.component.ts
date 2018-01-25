44c558773563c30d49988e8a9ea104e94f5a1488
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-01-25 11:57:00 -0600

    test
    test

--- src/app/grafico/grafico.component.ts
+++ src/app/grafico/grafico.component.ts
@@ -1,21 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ServGraficoService } from ./serv-grafico.service
 
 @Component({
   selector: 'grafico',
   templateUrl: './grafico.component.html',
-  styleUrls: ['./grafico.component.css']
+  styleUrls: ['./grafico.component.css'],
+  providers: [ServGraficoService]
 })
 export class GraficoComponent implements OnInit {
 
-  constructor() { }
-
+  chartsData: GraficoDatos[]
+  constructor(private chartService: ServGraficoService) { 
+  console.log('constructor Chart');
   ngOnInit() {
+
+  	console.log('ngOnInit start');
+    const _pieChartData: Array<any> = new Array();
+    this.chartService
+     .getDatosGrap()
+     .then((chartsData: GraficoDatos[]) => {
+      setTimeout( () => {
+
+      	console.log(GraficoDatos);
+        this.chartsData =chartsData.map((chartdemo) => {  
+        console.log('chartdemo: ',chartdemo.porciento); 
+        console.log('chartdemo: ',chartdemo.nombre);
+        _pieChartData.push(chartdemo.porciento);
+        this.pieChartData=_pieChartData;
+        this.pieChartLabels.push(chartdemo.nombre);
+        console.log('this.pieChartData.length: ',this.pieChartData.length);
+        console.log('this.pieChartLabels.length: ',this.pieChartLabels.length);
+        console.log('this.pieChartLabels',this.pieChartLabels);
+        return chartdemo;
+       });
+      });
+     })
+     .catch((err) => console.log("Error:", err));
+
+
   }
 
-
-  public pieChartLabels:string[] = ['Acuerdo', 'Acuerdo en parte', 'Desacuerdo en parte', 'Desacuerdo', 'No deseo responder'];
-  public pieChartData:number[] = [100, 130, 300, 450,30];
-  public pieChartType:string = 'pie';
 
 
 }
