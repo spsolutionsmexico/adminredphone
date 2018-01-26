f7afff9521f99860ecf650939cc39b4124e82adc
arturogonza <agonzalez@spsolutions.com.mx>
2018-01-25 23:02:28 -0600

    final
    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { GraficoComponent }   from './grafico/grafico.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'resultados', pathMatch: 'full' }
   { path: 'Procesos', component:  ActualizarCompComponent },
   { path: 'resultados', component: GraficoComponent }
 ];
