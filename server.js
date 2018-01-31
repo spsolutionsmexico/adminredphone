af14234afca102cd6fb89e059d5cb29cf2c1a2c0
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-01-31 13:25:17 -0600

    1
    1

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function extraerespuestas(callback) {
 
         console.log('respuesta', respuesta);
         console.log('arrId:', arrId);
-        callback(null, respuesta);
+        callback(respuesta);
     });
 }
 
