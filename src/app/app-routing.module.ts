7ac6af4b4eff225c735a377fde4437b8dfd5bda6
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-02-19 19:10:38 -0600

    1
    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule }             from '@angular/core';
+import { NgModule, Component }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
@@ -16,8 +16,9 @@ import { NavbarComponent } from 'app/navbar/navbar.component';
 const AppRoutingModule: Routes = [
   { 
     path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
+    redirectTo: 'login',
+    pathMatch: 'full',
+    component: LoginComponent,
   },
   {
     path: 'resultados',
