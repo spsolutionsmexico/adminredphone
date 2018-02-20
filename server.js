f2423e57a1fd1a5ffbaf55dd6daec2f2ba0fe9f0
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-02-19 18:15:08 -0600

    test

--- server.js
+++ server.js
@@ -396,7 +396,7 @@ app.get("/api/respuestas/:idreto", function(req, res) {
 
 //Redirecciona los f5
 app.get('*', function(req, res) {
-    res.send('./src/app/app.component')
+    res.send('./src/app/app-routing.module')
 })
 
 
