bde252c2593e42dc655154b7871981bd1bd7dc16
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-02-20 17:12:18 -0600

    4

--- src/app/actualizar/actualizar-comp/actualizar-comp.component.ts
+++ src/app/actualizar/actualizar-comp/actualizar-comp.component.ts
@@ -34,7 +34,8 @@ export class ActualizarCompComponent implements OnInit {
   public hecho: boolean = false;
 
   public actualizarReto(): void {
-    this.actualizarservice.getActualizar;
+    console.log("Call actualizar button");
+    this.actualizarservice.getActualizar();
     this.hecho = false;
     console.log("Call actualizar reto button");
     this.actualizarservice.getActualizarReto(this.idreto);
